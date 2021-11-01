5d98f4c5af473222eb1a19619f5cda761ea8bb8b
Rohityadav19 <rohitteja19@gmail.com>
2021-11-01 13:10:09 +0530

    Added Swagger

--- src/main/java/com/ecommerce/user/UserApplication.java
+++ src/main/java/com/ecommerce/user/UserApplication.java
@@ -1,13 +1,37 @@
 package com.ecommerce.user;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class UserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
 	}
+	@Bean
+    public Docket swaggerConfig() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.ecommerce")).build().apiInfo(apiDetails());
+    }
+    
+    public ApiInfo apiDetails() {
+        return new ApiInfo("User-Service API", "This is an api of authorization in Ecommerce Application", "1.0",
+                "Authorization Api", new Contact("Neeraj", null, "xyz@gmail.com"), "license", "http://localhost:9092",
+                Collections.emptyList());
+    }
 
 }
--- src/main/java/com/ecommerce/user/controller/UserControllerImpl.java
+++ src/main/java/com/ecommerce/user/controller/UserControllerImpl.java
@@ -20,7 +20,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 @RequestMapping("/fasscio")
 public class UserControllerImpl implements UserController {
 
